ed9b8f78e6067e3d9d76de5edd1c4b1c858a39a3
Paolo <paronne@servoy.com>
2018-01-24 17:16:50 +0200

    #3 fix: When I click on the day cell at left(small calendar) the callback function return me a day before(on the right calendar the date is correct)

--- fullcalendarComponent$demo/forms/demoCalendar.js
+++ fullcalendarComponent$demo/forms/demoCalendar.js
@@ -213,7 +213,7 @@ function onLoad(event) {
 				day: 'dddd M/D'
 			},
 			dayNamesShort : dayNamesShort,
-			defaultDate : new Date(2016, 4, 1),
+			defaultDate : new Date(2016, 3, 27),
 			defaultView: fullCalendar.CALENDAR_VIEW_TYPE.AGENDAWEEK,
 			editable: true,
 			eventSources : eventSources,
@@ -229,7 +229,7 @@ function onLoad(event) {
 			nowIndicator: true,
 			scrollTime: "8:00:00",
 			selectable: true,
-			selectConstraint: 'businessHours',
+//			selectConstraint: 'businessHours',
 //			timeFormat: {
 //				agenda: 'h:mm',
 //				'': 'h(:mm)t'
@@ -245,6 +245,7 @@ function onLoad(event) {
 	
 	// init the calendar in the left pane for date selection
 	elements.fullcalendarSelector.fullCalendar({
+		defaultDate : new Date(2016, 3, 27),
 		firstDay: 1,
 		selectable: false,
 		editable: false,
@@ -498,6 +499,7 @@ function resourceEventSourceCallback(start, end, params) {
 		var event = getEvent(record);
 		retval.push(event)
 	}
+	
 	return retval;
 }
 
